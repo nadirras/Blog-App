507cb29c260f827d9ccf035c2fbd8c2dbc26246c
nadirras <75488956+nadirras@users.noreply.github.com>
2024-04-26 08:22:02 +0700

    add router

--- src/routers/author.router.ts
+++ src/routers/author.router.ts
@@ -5,6 +5,7 @@ import {
   deleteAuthor,
   getAuthor,
   getAuthorById,
+  keepLogin,
   updateAuthor,
   verifyAuthor,
 } from "../controllers/author.controller";
@@ -15,6 +16,7 @@ authorRouter.post("/", createAuthor);
 authorRouter.post("/login", authorLogin);
 authorRouter.get("/");
 authorRouter.get("/verify", verifyToken, verifyAuthor);
+authorRouter.get("/keep-login", verifyToken, keepLogin);
 authorRouter.get("/:id", getAuthorById);
 authorRouter.patch("/:id", updateAuthor);
 authorRouter.delete("/:id", deleteAuthor);
